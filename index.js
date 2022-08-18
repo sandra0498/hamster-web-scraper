3510d5151e230a26b775059b4e1702623f750384
Sandra Chavez <49805900+sandra0498@users.noreply.github.com>
2022-08-17 17:41:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const puppeteer = require('puppeteer');
 
 
-// function to get the raw data
-const getRawData = (URL) => {
-   return fetch(URL)
-      .then((response) => response.text())
-      .then((data) => {
-         return data;
-      });
-};
 
 // URL for data
 const URL = "https://canmypeteatthis.com/what-can-hamsters-eat-your-ultimate-list/"
 
-// const URL = "";
 
 
 const getFoodName = (title) => {
@@ -28,6 +19,7 @@ const getFoodName = (title) => {
 
 
 }
+
 
 async function scrapeData() {
 
@@ -61,21 +53,6 @@ async function scrapeData() {
 
 
       }
-      // const [title] = await page.$x('//*[@id="lcp_instance_0"]/li[1]/a[1]');
-
-      // const  srcTitle = await title.getProperty('textContent');
-      // const titleTxt = await srcTitle.jsonValue(); 
-
-      // const foodName = getFoodName(titleTxt); 
-
-      // const [el] = await page.$x('//*[@id="lcp_instance_0"]/li[1]/div[1]');
-
-      // const src = await el.getProperty('textContent');
-
-      // const srcTxt = await src.jsonValue(); 
-
-
-      // console.log({foodName, srcTxt});
 
 
      browser.close(); 
@@ -89,83 +66,6 @@ async function scrapeData() {
    }
 }
 
-// start of the program
-const getFoodList = async () => {
-   const FoodData= await getRawData(URL);
 
-   console.log(typeof FoodData);
-   // const parser = new DOMParser();
-   // const htmlDoc = parser.parseFromString(FoodData, 'text/html');
-
-   const list = `$("ul")`;
-   console.log(list.length); 
-
-   // parsing the data
-   const parsedFoodData = load(FoodData)('content');
-
-   const parentTag = `<ul id="lcp_catlist" id="lcp_instance_0">`; 
-   const dom = new JSDOM(parentTag); 
-
-   console.log(dom.window.document.textContent);
-
-   const index = FoodData.indexOf('<div id="content" class="site-content clr">');
-   console.log(index);
-   
-   const parsed = FoodData.substring(index + 1); 
-   // console.log(parsed);
-
-   
-   // console.log(newData);
-
-   // // extracting the table data
-   // const worldCupsDataTable = parsedCricketWorldCupData("table.wikitable")[0]
-   //    .children[1].children;
-
-   // console.log("Year --- Winner --- Runner");
-   // worldCupsDataTable.forEach((row) => {
-   //    // extracting `td` tags
-   //    if (row.name === "tr") {
-   //       let year = null,
-   //          winner = null,
-   //          runner = null;
-
-   //       const columns = row.children.filter((column) => column.name === "td");
-
-   //       // extracting year
-   //       const yearColumn = columns[0];
-   //       if (yearColumn) {
-   //          year = yearColumn.children[0];
-   //          if (year) {
-   //             year = year.children[0].data;
-   //          }
-   //       }
-
-   //       // extracting winner
-   //       const winnerColumn = columns[3];
-   //       if (winnerColumn) {
-   //          winner = winnerColumn.children[1];
-   //          if (winner) {
-   //             winner = winner.children[0].data;
-   //          }
-   //       }
-
-   //       // extracting runner
-   //       const runnerColumn = columns[5];
-   //       if (runnerColumn) {
-   //          runner = runnerColumn.children[1];
-   //          if (runner) {
-   //             runner = runner.children[0].data;
-   //          }
-   //       }
-
-   //       if (year && winner && runner) {
-   //          console.log(`${year} --- ${winner} --- ${runner}`);
-   //       }
-   //    }
-   // });
-};
-
-// invoking the main function
-// getFoodList();
 
 scrapeData();
